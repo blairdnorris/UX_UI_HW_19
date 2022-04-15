b9d319615cbe290ac24ed77eae8d2da57e3a349e
Blair Norris <bdnorris23@gmail.com>
2022-04-14 18:37:52 -0600

    HW19

---
+++ index.js
@@ -0,0 +1,24 @@
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    var modal = document.getElementById("myModal");
+
+    var btn = document.getElementById("myBtn");
+
+    var span = document.getElementsByClassName("close") [0];
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+    
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
